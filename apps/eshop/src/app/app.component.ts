5180041bc0d244f7df837f084e4a7e86a6755572
birendra <bistbirendra2017@gmail.com>
2022-06-21 18:24:53 +0200

    initial commit

---
+++ apps/eshop/src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'zibat-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss'],
+})
+export class AppComponent {
+  title = 'eshop';
+}
